79111009c10816c07fbca1f25e7f6c3d08c3546d
kukita123 <kukita@abv.bg>
2026-03-19 14:35:56 +0200

    Added some new methods

--- 12.03.2026-uprProgramirane/Program.cs
+++ 12.03.2026-uprProgramirane/Program.cs
@@ -18,7 +18,91 @@ namespace _12._03._2026_uprProgramirane
     class Program
     {
         #region 9a
-        //public static void CreateRandomArray(int[] array)
+        public static void CreateRandomArray(int[] array)
+        {
+            Random r = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(-50, 50);
+            }
+        }
+        public static void InputArrayValues(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Enter next element: ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        public static int[] CreateArray(int n)
+        {
+            int[] array = new int[n];
+            Random r = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(-100, 100);
+            }
+            return array;
+        }
+        static int SumArray(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+        static double AverageArray(int[] array)
+        {
+            return (double)SumArray(array) / array.Length;
+        }
+        public static void DisplayArray(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine("Next element: {0}", item);
+            }
+        }
+        public static void DisplayElements(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Element [{0}] = {1}", i, array[i]);
+            }
+        }
+        public static int Min(int[] array)
+        {
+            int min = array[0];  //min = int.MaxValue и for трябва да започва от 0
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                    min = array[i];
+            }
+            return min;
+        }
+        public static int Max(int[] array)
+        {
+            int max = array[0]; //max = int.MinValie и for трябва да започва от 0
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                    max = array[i];
+            }
+            return max;
+        }
+        public static int[] ReverceArray(int[] array)
+        {
+            int[] newArray = new int[array.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[array.Length - 1 - i];
+            }
+            return newArray;
+        }
+        #endregion
+        #region 9b
+        //public static void StoreRandomData(int[] array)
         //{
         //    Random r = new Random();
         //    for (int i = 0; i < array.Length; i++)
@@ -26,147 +110,111 @@ namespace _12._03._2026_uprProgramirane
         //        array[i] = r.Next(-50, 50);
         //    }
         //}
-        //public static void InputArrayValues(int[] array)
-        //{            
+        //public static void StoreConsoleData(int[] array)
+        //{
+        //    Console.WriteLine("Enter array values from the console:");
         //    for (int i = 0; i < array.Length; i++)
         //    {
-        //        Console.Write("Enter next element: ");
+        //        Console.Write($"array[{i}] = ");
         //        array[i] = int.Parse(Console.ReadLine());
         //    }
         //}
-        //public static int[] CreateArray(int n)
+        //public static void DisplayArray(int[] array)
         //{
-        //    int[] array = new int[n];
+        //    Console.WriteLine("Array elements: ");
+        //    foreach (var item in array)            
+        //    {               
+        //        Console.WriteLine(item);
+        //    }
+        //}
+        //public static void DsplayArrayWithIndexes(int[] array)
+        //{
+        //    Console.WriteLine("Array elements:");
+        //    for (int i = 0; i < array.Length; i++)
+        //    {
+        //        Console.WriteLine($"array[{i}] = {array[i]}");
+        //    }
+        //}
+        //public static int[] CreateRandomArray(int length)
+        //{
+        //    int[] array = new int[length];
+
         //    Random r = new Random();
         //    for (int i = 0; i < array.Length; i++)
         //    {
-        //        array[i] = r.Next(-100, 100);
+        //        array[i] = r.Next(-50, 50);
         //    }
         //    return array;
         //}
-        //static int SumArray(int[] array)
+        //public static int Sum(int[] array)
         //{
         //    int sum = 0;
-        //    for (int i = 0; i < array.Length; i++)
+        //    foreach (var item in array)
         //    {
-        //        sum += array[i];
+        //        sum += item;
         //    }
         //    return sum;
         //}
-        //public static void DisplayArray(int[] array)
-        //{
-        //    foreach (var item in array)
-        //    {
-        //        Console.WriteLine("Next element: {0}", item);
-        //    }
-        //}
-        #endregion
-        #region 9b
-        public static void StoreRandomData(int[] array)
-        {
-            Random r = new Random();
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = r.Next(-50, 50);
-            }
-        }
-        public static void StoreConsoleData(int[] array)
-        {
-            Console.WriteLine("Enter array values from the console:");
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write($"array[{i}] = ");
-                array[i] = int.Parse(Console.ReadLine());
-            }
-        }
-        public static void DisplayArray(int[] array)
-        {
-            Console.WriteLine("Array elements: ");
-            foreach (var item in array)            
-            {               
-                Console.WriteLine(item);
-            }
-        }
-        public static void DsplayArrayWithIndexes(int[] array)
-        {
-            Console.WriteLine("Array elements:");
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.WriteLine($"array[{i}] = {array[i]}");
-            }
-        }
-        public static int[] CreateRandomArray(int length)
-        {
-            int[] array = new int[length];
-
-            Random r = new Random();
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = r.Next(-50, 50);
-            }
-            return array;
-        }
-        public static int Sum(int[] array)
-        {
-            int sum = 0;
-            foreach (var item in array)
-            {
-                sum += item;
-            }
-            return sum;
-        }
         #endregion
         static void Main(string[] args)
         {
             #region 9a
-            //Console.Write("Enter array count: ");
-            //int count = int.Parse(Console.ReadLine());
+            Console.Write("Enter array count: ");
+            int count = int.Parse(Console.ReadLine());
 
-            //int[] array1 = new int[count];
-            //CreateRandomArray(array1);
-            //Console.WriteLine("First array:");            
+            int[] array1 = new int[count];
+            CreateRandomArray(array1);
+            Console.WriteLine("First array:");
+            DisplayElements(array1);
+            Console.WriteLine("Min = {0}", Min(array1));
+            Console.WriteLine("Max = {0}", Max(array1));
+            int[] revercedArray = ReverceArray(array1);
+            DisplayElements(revercedArray);
+
+
             //DisplayArray(array1);
-            //Console.WriteLine("Elements sum: " + SumArray(array1));
+            Console.WriteLine("Elements sum: " + SumArray(array1));
+            Console.WriteLine("Elements average: "+ AverageArray(array1));
 
-            //int[] array2 = new int[10];
-            //CreateRandomArray(array2);
-            //Console.WriteLine("Second array:");
-            //DisplayArray(array2);
-            //Console.WriteLine("Elements sum: " + SumArray(array2));
-            ////int[] array3 = new int[count];
-            ////InputArrayValues(array3);
-            ////Console.WriteLine("Third array:");
-            ////DisplayArray(array3);
+            int[] array2 = new int[10];
+            CreateRandomArray(array2);
+            Console.WriteLine("Second array:");
+            DisplayArray(array2);
+            Console.WriteLine("Elements sum: " + SumArray(array2));
+            //int[] array3 = new int[count];
+            //InputArrayValues(array3);
+            //Console.WriteLine("Third array:");
+            //DisplayArray(array3);
 
-            //int[] array4 = CreateArray(4);
-            //Console.WriteLine("Forth array:");
-            //DisplayArray(array4);
-            //Console.WriteLine("Elements sum: " + SumArray(array4));
+            int[] array4 = CreateArray(4);
+            Console.WriteLine("Forth array:");
+            DisplayArray(array4);
+            Console.WriteLine("Elements sum: " + SumArray(array4));
             #endregion
             #region 9b
-            int[] arr = CreateRandomArray(7);
-            DsplayArrayWithIndexes(arr);
-            Console.WriteLine("Sum array elements: {0}", Sum(arr));
-            Console.WriteLine();
+            //int[] arr = CreateRandomArray(7);
+            //DsplayArrayWithIndexes(arr);
+            //Console.WriteLine("Sum array elements: {0}", Sum(arr));
+            //Console.WriteLine();
 
-            int[] array1 = new int[5];
-            StoreRandomData(array1);
-            DsplayArrayWithIndexes(array1);
-            Console.WriteLine("Sum array elements: {0}", Sum(array1));
-            Console.WriteLine();
+            //int[] array1 = new int[5];
+            //StoreRandomData(array1);
+            //DsplayArrayWithIndexes(array1);
+            //Console.WriteLine("Sum array elements: {0}", Sum(array1));
+            //Console.WriteLine();
 
-            Console.Write("Enter second array length: ");
-            int n = int.Parse(Console.ReadLine());
-            int[] array2 = new int[n];
-            StoreRandomData(array2);
-            DisplayArray(array2);
-            Console.WriteLine("Sum array elements: {0}", Sum(array2));
-            Console.WriteLine();
+            //Console.Write("Enter second array length: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array2 = new int[n];
+            //StoreRandomData(array2);
+            //DisplayArray(array2);
+            //Console.WriteLine("Sum array elements: {0}", Sum(array2));
+            //Console.WriteLine();
 
-            int[] array3 = new int[3];
-            StoreConsoleData(array3);
-            DisplayArray(array3);
-            Console.WriteLine("Sum array elements: {0}", Sum(array3));
+            //int[] array3 = new int[3];
+            //StoreConsoleData(array3);
+            //DisplayArray(array3);
+            //Console.WriteLine("Sum array elements: {0}", Sum(array3));
             #endregion
 
         }
